b519cd260259ad2f5ded27a8a043b2a52b316419
Jesus <alv.mtz94@gmail.com>
2018-09-20 23:02:58 +0200

    created basic functionality of the app

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class RandomQuoteMachine extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      quote: '',
+      author: ''
+    }
+
+    this.handleNewQuote = this.handleNewQuote.bind(this);
+    this.handleTweetQuote = this.handleTweetQuote.bind(this);
+  }
+
+  componentDidMount() {
+    const self = this;
+    fetch('https://talaikis.com/api/quotes/random/')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+      self.setState({
+        quote: myJson.quote,
+        author: myJson.author
+      });
+    });
+  }
+
+  handleNewQuote() {
+    console.log(this);
+    const self = this;
+    fetch('https://talaikis.com/api/quotes/random/')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+      self.setState({
+        quote: myJson.quote,
+        author: myJson.author
+      });
+    });
+  }
+
+  handleTweetQuote() {
+    const txtToTweet = this.state.quote + " - " + this.state.author;
+    const tweetLink = 'http://twitter.com/home?status=' + encodeURIComponent(txtToTweet);
+    window.open(tweetLink);
+  }
+
+  render(){
+    
+    return (
+      <div id="quote-box">
+        <p id="text">{this.state.quote}</p>
+        <h1 id="author">{this.state.author}</h1>
+        <button onClick={this.handleNewQuote} id="new-quote">New Quote</button>
+        <button onClick={this.handleTweetQuote} id="tweet-quote">Tweet Quote</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <RandomQuoteMachine />, 
+  document.getElementById('root')
+);
+
